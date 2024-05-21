14ac90e435ae4e64aadadd6a81bbe0b4881fa236
polarnaldo <polarnaldo06@gmail.com>
2024-05-21 12:38:12 +0200

    Update Run Docker

--- run-docker.sh
+++ run-docker.sh
@@ -32,7 +32,7 @@ docker run -dit --name USPython -v /var/run/docker.sock:/var/run/docker.sock -v 
 docker exec USPython bash -c "apt-get update && apt-get install -y sudo git docker-compose python3 python3-yaml"
 
 # Clone the specified GitHub repository into the container
-docker exec USPython git clone https://github.com/polarnaldo/USPython
+docker exec USPython bash -c "git clone https://github.com/polarnaldo/USPython"
 
 # Run the specified Python script within the container
-docker exec -it USPython python3 /USPython/USPython.py -m interface
+docker exec -it USPython bash -c "python3 /USPython/USPython.py -m interface"
