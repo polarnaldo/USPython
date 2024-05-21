54aee59ebb3a2a852465b5898a4e7b374611d527
polarnaldo <polarnaldo06@gmail.com>
2024-05-21 10:45:49 +0200

    Update Run Docker

--- run-docker.sh
+++ run-docker.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# USPython - Author: @polarnaldo (Pol Arnaldo)
+
+# CTRL+C
+
+function ctrl_c (){
+  echo -e "${redColour}\n\n[!] Exiting...\n${endColour}"
+  tput cnorm && exit 1
+}
+
+trap ctrl_c INT
+
+# COLOURS
+
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
 # Pull the latest Ubuntu image
 docker pull ubuntu:latest
 
